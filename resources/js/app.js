c9a03a980d0ca4c9b629b8bfa6985a6a429f642e
Albert Melo <albert.uler@brilliantmachine.com.br>
2024-07-08 18:04:11 -0300

    Substituindo template em tailwind por vuetify com sucesso

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,15 @@
 import './bootstrap';
 import '../css/app.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import 'vuetify/styles';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createVuetify } from 'vuetify';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -12,9 +17,19 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        const configVuetify = {
+            components,
+            directives,
+            icons: {
+                defaultSet: 'mdi'
+            }
+        };
+        const vuetify = createVuetify(configVuetify);
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
